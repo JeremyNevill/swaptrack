16b667e91252a24241183ee2cae680f23178e63b
Jeremy Nevill <jeremy@nevill.net>
2014-06-12 21:47:13 +0000

    First working firebase connection

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,10 +8,24 @@
  * Controller of the badgerApp
  */
 angular.module('badgerApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+    .controller('MainCtrl', function ($scope, $firebase) {
+
+        var peopleRef = new Firebase("https://badger.firebaseio.com/people");
+        $scope.people = $firebase(peopleRef);
+
+
+        $scope.people = $firebase(peopleRef);
+        $scope.addPerson = function () {
+            $scope.people.$add($scope.newPerson);
+            $scope.newPerson = "";
+        }
+
+
+        $scope.awesomeThings = [
+            'HTML5 Boilerplate',
+            'AngularJS',
+            'Karma'
+        ];
+
+
+    });
